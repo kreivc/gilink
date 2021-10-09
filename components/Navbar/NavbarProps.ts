6da61c672e7502abca7486532eaa609cfb801a9d
Ricky <phangr28@gmail.com>
2021-10-10 02:01:32 +0700

    testing export props

---
+++ components/Navbar/NavbarProps.ts
@@ -0,0 +1,5 @@
+export const flex1 = {
+	justifyContent: "space-between",
+	alignItems: "center",
+	p: "2",
+};
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Box, Flex, Text } from "@chakra-ui/react";
+import { flex1 } from "./NavbarProps";
 
 export default function Navbar() {
 	return (
 		<Box px="5">
-			<Flex justifyContent={"space-between"} alignItems={"center"} p="2">
+			<Flex {...flex1}>
 				<Text fontSize={"2xl"}>gilink</Text>
 				<Text fontWeight={"semibold"} fontSize={"2xl"} textAlign={"center"}>
 					Navbar
