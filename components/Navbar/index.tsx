0e5b51fad6a1a8dd00c84fef0ff97aef4cb4f4f3
Ricky <phangr28@gmail.com>
2021-10-10 20:10:33 +0700

    add prisma init

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,40 +1,39 @@
-import React from 'react';
-import { Flex, Text } from '@chakra-ui/react';
-import { logoTextProps, parentContainerProps } from './style-props';
-import { signIn, signOut, useSession } from 'next-auth/client';
+import React from "react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { logoTextProps, parentContainerProps } from "./style-props";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 function Session() {
-    const [session, loading] = useSession();
+	const [session, loading] = useSession();
 
-    return (
-        <>
-            {!session && (
-                <>
-                    Not signed in <br />
-                    <button onClick={() => signIn()}>Sign in</button>
-                </>
-            )}
-            {session && (
-                <>
-                    Signed in as {session.user.name} <br />
-                    <button onClick={() => signOut()}>Sign out</button>
-                </>
-            )}
-        </>
-    );
+	return (
+		<>
+			{!session ? (
+				<Flex alignItems={"center"} gridGap={"3"}>
+					<Text>Not signed in</Text>
+					<Button colorScheme={"brand"} onClick={() => signIn()}>
+						Sign in
+					</Button>
+				</Flex>
+			) : (
+				<Flex alignItems={"center"} gridGap={"3"}>
+					<Text>Signed in as {session.user.name}</Text>
+					<Button colorScheme={"brand"} onClick={() => signOut()}>
+						Sign out
+					</Button>
+				</Flex>
+			)}
+		</>
+	);
 }
 
 export default function Navbar() {
-    return (
-        <Flex {...parentContainerProps}>
-            <Text {...logoTextProps}>gilink</Text>
-            {/* Navbar?? */}
-            <Text fontWeight={'semibold'} fontSize={'2xl'} textAlign={'center'}>
-                Navbar
-            </Text>
-            <Text>
-                <Session />
-            </Text>
-        </Flex>
-    );
+	return (
+		<Flex {...parentContainerProps}>
+			<Text {...logoTextProps}>gilink</Text>
+			<Text>
+				<Session />
+			</Text>
+		</Flex>
+	);
 }
