3bf369da9cf393ccf1f03d1b1ab84967daa6f441
ayangd <jimmyhendrixx90@gmail.com>
2021-10-11 09:06:49 +0700

    Add login type and interface

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,39 +1,46 @@
-import { Box, Button, Input } from "@chakra-ui/react";
-import { useEffect } from "react";
-import Router from "next/router";
-import { providers, getSession } from "next-auth/client";
-import { NextPage } from "next";
-import { signIn } from "next-auth/client";
-import { NextPageContext } from "next";
+import { Box, Button, Input } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import Router from 'next/router';
+import { providers, getSession, ClientSafeProvider } from 'next-auth/client';
+import { signIn } from 'next-auth/client';
+import { NextPageContext } from 'next';
+import { Session } from 'next-auth';
 
-const Login: NextPage = ({ providers, session }) => {
-	useEffect(() => {
-		if (session) return Router.push("/");
-	}, [session]);
+export interface LoginProps {
+    providers: Record<string, ClientSafeProvider>;
+    session: Session;
+}
 
-	if (session) return null;
+function Login({ providers, session }: LoginProps) {
+    useEffect(() => {
+        if (session) {
+            Router.push('/');
+        }
+    }, [session]);
 
-	return (
-		<Box p="20">
-			<Input placeholder="email" type="email" />
-			<Input placeholder="password" type="password" />
-			<Box>
-				<Button
-					style={{ background: "brand", color: "brand.50" }}
-					onClick={() => signIn(providers.id)}
-				>
-					Sign in with {providers.name}
-				</Button>
-			</Box>
-		</Box>
-	);
-};
+    if (session) return null;
+
+    return (
+        <Box p="20">
+            <Input placeholder="email" type="email" />
+            <Input placeholder="password" type="password" />
+            <Box>
+                <Button
+                    style={{ background: 'brand', color: 'brand.50' }}
+                    onClick={() => signIn(providers.id)}
+                >
+                    Sign in with {providers.name}
+                </Button>
+            </Box>
+        </Box>
+    );
+}
 
 Login.getInitialProps = async (context: NextPageContext) => {
-	return {
-		providers: await providers(),
-		session: await getSession(context),
-	};
+    return {
+        providers: await providers(),
+        session: await getSession(context),
+    };
 };
 
 export default Login;
